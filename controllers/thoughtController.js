97d66d1966477b0014cb0eafc3f1437011994ebf
TyGosley <tygosley@gmail.com>
2023-04-20 18:12:13 -0700

    fixed thoughtController.js getAllThoughts route.  locolhost now working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,7 +2,7 @@ const { User, Thought } = require("../models");
 
 // /api/users
 const thoughtController = {
-getThought(req, res) {
+getAllThoughts(req, res) {
     Thought.find({})
     .select("-__v")
     .then((dbThoughtData) => res.json(dbThoughtData))
